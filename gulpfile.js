3cecaa99c696891cf01d289a27a82d78b7763b26
Roland Kullet <renaud.bellec.3@gmail.com>
2016-11-14 18:31:27 +0100

    configured js env

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,9 @@ require('laravel-elixir-vue-2');
 
 elixir((mix) => {
     mix.sass('app.scss')
-       .webpack('app.js');
+       .webpack('app.js')
+       .webpack(
+           './resources/assets/js/lib/app.js',
+           './public/js/dist'
+       );
 });
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>FB APP</title>
+    </head>
+    <body>
+        <h1>Toto</h1>
+        <script src="{{ elixir('js/dist/bundle.js') }}"></script>
+    </body>
+</html>
