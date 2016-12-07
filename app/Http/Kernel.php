6cea0a226248c0ec57c05fd16b6e2f846f04d1da
GoRFy <lambot.rom@gmail.com>
2016-12-07 12:44:43 +0100

    patch kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,8 +31,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        
-        'checkAdmin' => \App\Htpp\Middleware\CheckIsAdmin::class,
 
         'api' => [
             'throttle:60,1',
@@ -54,5 +52,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'checkAdmin' => \App\Http\Middleware\CheckIsAdmin::class,
     ];
 }
