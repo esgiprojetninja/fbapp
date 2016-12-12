b01e6a5eea9cd61a3ef0ff20266d0a22ef8917e9
Roland Kullet <renaud.bellec.3@gmail.com>
2016-12-12 17:04:28 +0100

    added Grid with pics

--- resources/assets/js/lib/pictureGame/ui/Gallery.js
+++ resources/assets/js/lib/pictureGame/ui/Gallery.js
@@ -1,26 +1,43 @@
 import React, {PropTypes as T} from "react";
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import Picture from "./Picture";
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  }
+};
 
 
 export default class Gallery extends React.PureComponent {
     render () {
         return (
-            <div>
+            <GridList
+                cellHeight={180}
+                style={styles.gridList}
+            >
                 {this.props.pictures.map((picture, index) => (
-                    <Picture url={picture.url} user={picture.user} key={index} />
+                    <GridTile
+                        subtitle={picture.url}
+                        title={picture.user}
+                        key={index}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src="https://static.pexels.com/photos/194087/pexels-photo-194087.jpeg" />
+                    </GridTile>
                 ))}
-            </div>
+            </GridList>
         )
-    }
-
-    renderPictures () {
-        return this.props.pictures.map(picture => {
-            this.renderPicture(picture);
-        });
-    }
-
-    renderPicture (picture) {
-        return <Picture url={picture.url} user={picture.user} />;
     }
 }
 
