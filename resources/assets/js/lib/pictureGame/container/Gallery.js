7614b99ae86cda5172459b11416ddb7e34e1fffd
tmeksava <teddy.meksavanh@arkena.com>
2017-01-05 12:18:18 +0100

    Starting responsive grid

--- resources/assets/js/lib/pictureGame/container/Gallery.js
+++ resources/assets/js/lib/pictureGame/container/Gallery.js
@@ -6,12 +6,54 @@ const mapStateToProps = (state) => {
         return {
            pictures: [
                {
-                   url: "http://toto.com",
-                   user: "toto"
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-1.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
                },
                {
-                   url: "http://tutu.com",
-                   user: "tutu"
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-5.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-4.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-3.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-1.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483549559-6.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-5.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-4.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-3.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
+               },
+               {
+                   src: "http://image.noelshack.com/fichiers/2017/01/1483545909-2.jpg",
+                   author: "Toto",
+                   title: "Tatouage tribal"
                }
            ]
        }
--- resources/assets/js/lib/pictureGame/ui/Gallery.js
+++ resources/assets/js/lib/pictureGame/ui/Gallery.js
@@ -1,27 +1,55 @@
 import React, {PropTypes as T} from "react";
-import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import Picture from "./Picture";
 
+// import { GridList, GridTile } from "material-ui/GridList";
+
+// <div>
+//     <GridList
+//         cellHeight={180}
+//         cols={4}
+//     >
+//     {this.props.pictures.map((picture, index) => (
+//         <GridTile
+//             key={index}
+//             title={picture.title}
+//             subtitle={<span>par <b>{picture.author}</b></span>}
+//             actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+//         >
+//             <img src={picture.src} />
+//         </GridTile>
+//     ))}
+//     </GridList>
+// </div>
+
 export default class Gallery extends React.PureComponent {
+    constructor(){
+        super();
+    }
+
     render () {
         return (
-            <GridList
-                cellHeight={180}
-            >
-                {this.props.pictures.map((picture, index) => (
-                    <GridTile
-                        subtitle={picture.url}
-                        title={picture.user}
-                        key={index}
-                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-                    >
-                        <img src="http://tattoospedia.com/wp-content/uploads/2013/12/Chest%20Tattoos/Tattoo%20Designs%20For%20Girls%20Chest%202.jpg" />
-                    </GridTile>
-                ))}
-            </GridList>
+            <div className="gridLayout">
+                <div className="gridContainer container">
+                    <div className="gridRow row">
+                        {this.props.pictures.map((picture, index) => (
+                        <div
+                            className="gridItem"
+                            key={index}
+                        >
+                            <div className="gridWell">
+                                <img
+                                    className="img-cover"
+                                    src={picture.src}
+                                />
+                            </div>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         )
     }
 }
@@ -29,8 +57,7 @@ export default class Gallery extends React.PureComponent {
 Gallery.propTypes = {
     pictures: T.arrayOf(
         T.shape({
-            url: T.string.isRequired,
-            user: T.string.isRequired
+            src: T.string.isRequired
         }).isRequired
     ).isRequired
 };
