6ba95fcf3f9885bfa70b58aa03e882ff82e20eec
GoRFy <lambot.rom@gmail.com>
2017-01-23 20:11:57 +0100

    Creating a Facebook

---
+++ app/Facebook.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Facebook
+{
+    protected $fb_app_id = '1200139990052440';
+    protected $fb_app_secret = '7ed0f55fd08612a805b851fa6fbde893';
+    protected $default_graph_version = 'v2.8';
+
+    protected $fb;
+
+    function __construct(){
+        $this->fb = new \Facebook\Facebook([
+            'app_id' => $this->fb_app_id,
+            'app_secret' => $this->fb_app_secret,
+            'default_graph_version' => $this->default_graph_version,
+        ]);
+    }
+
+    public function get(){
+        return $this->fb->get('/app/roles', "1200139990052440|sIs-ANSRKPtTyImEdl68B8P56ZI");
+    }
+
+}
