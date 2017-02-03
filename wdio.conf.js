7bef5a339c36d052c38803a5caef35b97cd9da65
Roland Kullet <renaud.bellec.3@gmail.com>
2017-02-03 14:27:25 +0100

    installed wdio with cucumber & first example test

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-    
+
     //
     // ==================
     // Specify Test Files
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './resources/assets/js/lib/pictureGame/__browser_tests__/'
+        './resources/assets/js/lib/pictureGame/__browser_tests__/features/**/*.feature'
     ],
     // Patterns to exclude.
     exclude: [
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'https://fbapp.dev',
+    baseUrl: 'https://esgi.ninja',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -121,23 +121,23 @@ exports.config = {
     // see also: http://webdriver.io/guide/testrunner/reporters.html
     // reporters: ['dot'],//
     // If you are using Cucumber you need to specify the location of your step definitions.
-    cucumberOpts: {
-        require: ['resources/assets/js/lib/pictureGame/__browser_tests__/steps/'],        // <string[]> (file/dir) require files before executing features
-        backtrace: false,   // <boolean> show full backtrace for errors
-        compiler: [],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-        dryRun: false,      // <boolean> invoke formatters without executing steps
-        failFast: false,    // <boolean> abort the run on first failure
-        format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
-        colors: true,       // <boolean> disable colors in formatter output
-        snippets: true,     // <boolean> hide step definition snippets for pending steps
-        source: true,       // <boolean> hide source uris
-        profile: [],        // <string[]> (name) specify the profile to use
-        strict: false,      // <boolean> fail if there are any undefined or pending steps
-        tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
-        timeout: 20000,     // <number> timeout for step definitions
-        ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
-    },
-    
+    // cucumberOpts: {
+    //     require: ['resources/assets/js/lib/pictureGame/__browser_tests__/'],        // <string[]> (file/dir) require files before executing features
+    //     backtrace: false,   // <boolean> show full backtrace for errors
+    //     compiler: [],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    //     dryRun: false,      // <boolean> invoke formatters without executing steps
+    //     failFast: false,    // <boolean> abort the run on first failure
+    //     format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    //     colors: true,       // <boolean> disable colors in formatter output
+    //     snippets: true,     // <boolean> hide step definition snippets for pending steps
+    //     source: true,       // <boolean> hide source uris
+    //     profile: [],        // <string[]> (name) specify the profile to use
+    //     strict: false,      // <boolean> fail if there are any undefined or pending steps
+    //     tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    //     timeout: 20000,     // <number> timeout for step definitions
+    //     ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
+    // },
+
     //
     // =====
     // Hooks
