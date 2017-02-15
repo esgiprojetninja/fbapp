8257354107aa406abd6f4bf782edc50077421552
dydyFos <dylanfoster.esgi@gmail.com>
2017-02-15 04:37:47 +0100

    css fixed on dropzone

--- resources/assets/js/lib/pictureGame/utils/FacebookLoader.js
+++ resources/assets/js/lib/pictureGame/utils/FacebookLoader.js
@@ -190,7 +190,8 @@ export default class FacebookLoader {
             type: "POST",
             data: fd,
             processData: false,
-            contentType: "jsonp",
+            /*######### CHANGE TO false for classic webbrowser: jsonp for app ######## */
+            contentType: false,
             cache: false,
         }).done(response => {
             callback(response);
